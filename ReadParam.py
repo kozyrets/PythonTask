4e24520ed52ec106bf65d9e367d0a80781f721b9
kozyrets <trancefer17@gmail.com>
2020-08-20 17:10:27 +0300

    Improved parameters reading

--- ReadParam.py
+++ ReadParam.py
@@ -1,16 +1,21 @@
 from openpyxl import load_workbook
 
+file_path = ''
+sheet_name = ''
 
-# Get ParamValue by ParamName
+
+# Get Param Value by Param Name
 def get_param_value(param_name):
+    global file_path, sheet_name
+
     # Load in the workbook
-    wb = load_workbook('./Param.xlsx')
+    wb = load_workbook(file_path)
 
     # Get a sheet by name
-    sheet = wb['Python']
+    sheet = wb[sheet_name]
 
     for row in range(sheet.min_row, sheet.max_row + 1):
-        if sheet.cell(row, 1).value == param_name:
+        if sheet.cell(row, 1).value.upper() == param_name.upper():
             cell_value = sheet.cell(row, 2).value
             return cell_value
             break
